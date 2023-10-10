cc9cb8c9e02bc4ddf2b6fbca01f35e1912bfe415
KEVIN-c60 <139542613+KEVIN-c60@users.noreply.github.com>
2023-10-10 08:19:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function greet(name) {
+    console.log("Hello, " + name + "!");
+  }
+  
+  // Calling the function
+  greet("John"); // Output: Hello, John!
+  greet("Jane"); // Output: Hello, Jane!
